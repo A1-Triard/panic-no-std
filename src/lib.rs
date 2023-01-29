44f67cdb21730e8ff6afa163bc7546df10508e8c
Warlock <internalmike@gmail.com>
2023-01-29 07:04:49 +0300

    code

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,63 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![no_std]
+
+use core::fmt::{self};
+use core::panic::PanicInfo;
+use exit_no_std::exit;
+
+#[cfg(all(not(target_os="dos"), windows))]
+fn write_ascii_char(c: u8) {
+    let stderr = GetStdHandle(STD_ERROR_HANDLE);
+    if stderr != null_mut() && stderr != INVALID_HANDLE_VALUE {
+        let mut written: WORD = 0;
+        WriteFile(stderr, &c as *const _ as _, 1, &mut written as *mut _, null_mut());
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[cfg(all(not(target_os="dos"), not(windows)))]
+fn write_ascii_char(c: u8) {
+    write(2, &c as *const _ as _, 1);
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[cfg(target_os="dos")]
+fn write_ascii_char(c: u8) {
+    if c == b'\n' {
+        int_21h_ah_02h_out_ch(b'\r');
     }
+    int_21h_ah_02h_out_ch(c);
+}
+
+struct LastChanceWriter;
+
+impl fmt::Write for LastChanceWriter {
+    fn write_char(&mut self, c: char) -> fmt::Result {
+        let c = c as u32;
+        let c = if c > 0x7F || c == '\r' as u32 {
+            b'?'
+        } else {
+            c as u8
+        };
+        write_ascii_char(c);
+        Ok(())
+    }
+
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for c in s.chars() {
+            self.write_char(c)?;
+        }
+        Ok(())
+    }
+}
+
+pub fn panic(exit_code: u8, info: &core::panic::PanicInfo) -> ! {
+    let _ = LastChanceWriter.write_str("Panic");
+    if let Some(&message) = info.message() {
+        let _ = write!(LastChanceWriter, ": {message}");
+    } else if let Some(message) = info.payload().downcast_ref::<&str>() {
+        let _ = write!(LastChanceWriter, ": {message}");
+    }
+    if let Some(location) = info.location() {
+        let _ = writeln!(LastChanceWriter, " ({location})");
+    }
+    let _ = writeln!(LastChanceWriter, ".");
+    exit(exit_code)
 }
