ef12de0927d92a2a66f195128bbd38d908957ffb
Warlock <internalmike@gmail.com>
2024-09-18 22:11:09 +0300

    update examples

--- examples/hello_in_panic/src/main.rs
+++ examples/hello_in_panic/src/main.rs
@@ -8,20 +8,9 @@
 #![cfg_attr(any(target_os="dos", windows), no_main)]
 
 extern crate pc_atomics;
-extern crate rlibc;
+extern crate rlibc_ext;
 
 mod no_std {
-    #[no_mangle]
-    extern "C" fn _aulldiv() -> ! { panic!("10") }
-    #[no_mangle]
-    extern "C" fn _aullrem() -> ! { panic!("11") }
-    #[no_mangle]
-    extern "C" fn _chkstk() { }
-    #[no_mangle]
-    extern "C" fn _fltused() -> ! { panic!("13") }
-    #[no_mangle]
-    extern "C" fn strlen() -> ! { panic!("14") }
-
     #[panic_handler]
     fn panic_handler(info: &core::panic::PanicInfo) -> ! { panic_no_std::panic(info, b'P') }
 }
