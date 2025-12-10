1db423b0129209ee19bb6ef6522c9077035b7735
Master <internalmike@gmail.com>
2025-12-10 19:37:54 +0300

    update ens

--- ensure_no_std/src/main.rs
+++ ensure_no_std/src/main.rs
@@ -1,22 +1,22 @@
-#![feature(start)]
-
 #![deny(warnings)]
 
 #![no_std]
+#![no_main]
 
+use core::ffi::{c_char, c_int};
 use core::panic::PanicInfo;
 use panic_no_std::panic;
 
 #[cfg(windows)]
 #[link(name="msvcrt")]
-extern { }
+extern "C" { }
 
 #[panic_handler]
 fn panic_hanlder(info: &PanicInfo) -> ! {
     panic(info, 0)
 }
 
-#[start]
-pub fn main(_argc: isize, _argv: *const *const u8) -> isize {
+#[unsafe(no_mangle)]
+extern "C" fn main(_argc: c_int, _argv: *mut *mut c_char) -> c_int {
     panic!("PANIC");
 }
